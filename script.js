f90db4b074f4723e9da854f919d0f8cf274c7e61
Mohit Phogat <mphogat007@gmail.com>
2024-05-26 22:23:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Set the target date for the countdown
+const targetDate = new Date('july 01, 2024').getTime();
+
+function updateCountdown() {
+    // Get the current date
+    const now = new Date().getTime();
+    
+    // Find the distance between now and the target date
+    const distance = targetDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Display the result in the element with id="launch"
+    document.getElementById("launch").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
